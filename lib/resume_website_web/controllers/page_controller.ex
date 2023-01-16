e535318e20008774200e1a492b8b9c832d1edf40
Matteo De Ponti <matteo.deponti@prima.it>
2023-01-16 09:03:24 +0100

    wip

--- lib/resume_website_web/controllers/page_controller.ex
+++ lib/resume_website_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule ResumeWebsiteWeb.PageController do
+  alias ResumeWebsiteWeb.ApiController
   use ResumeWebsiteWeb, :controller
 
   def index(conn, _params) do
+
+    bio = ApiController.get_bio(conn, %{"language_id"=>"it"})
+
     render(conn, "index.html")
   end
 end
